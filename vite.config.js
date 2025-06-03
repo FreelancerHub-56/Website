df3a04f457c31d61797da56fec045bcd5b54f7ce
akshay18-tech <akshay789597@gmail.com>
2025-06-03 21:44:22 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,5 +6,5 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
-  base: './', // This is important if deploying to Vercel
+  base: './'
 })
